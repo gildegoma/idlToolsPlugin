5ba2ab6490742972faf4d8169e81a10a61ef6cb0
David Jeanmonod <david.jeanmonod@idael.ch>
2009-12-17 16:13:32 +0000

    Url::Cleanup upèdate

--- lib/functions/idlUrl.class.php
+++ lib/functions/idlUrl.class.php
@@ -3,10 +3,24 @@
 class idlUrl extends idlFunction {
   
   public static function cleanup($url){
-  
-    return str_replace(" ","%20", $url);
-  
+    $cleanUrl = "";
+    $parts = parse_url($url);
+    $cleanUrl .= $parts['scheme'].'://'.$parts['host'];
+    if (isset($parts['path'])){
+      $path = substr($parts['path'],1);
+      $pathParts = explode('/', $path);
+      foreach ($pathParts as $pathPart){
+        $cleanUrl .= '/' . rawurlencode($pathPart);
+      }  
+    }
+    
+    if (isset($parts['query'])){
+      $cleanUrl .= '?' . rawurlencode($parts['query']);
+    }
+    if (isset($parts['fragment'])){
+      $cleanUrl .= '#' . rawurlencode($parts['fragment']);
+    }
+    return $cleanUrl;
   }
-  
 }
   

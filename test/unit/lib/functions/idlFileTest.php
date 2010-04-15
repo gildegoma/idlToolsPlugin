175bfefe25359b26890e2bae42378fae81915ca8
David Jeanmonod <david.jeanmonod@idael.ch>
2010-04-15 07:46:50 +0000

    Temporary commit for idlFile and idlFolder test

---
+++ test/unit/lib/functions/idlFileTest.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once(dirname(__FILE__).'/../../../bootstrap/unit.php');
+//include_once(dirname(__FILE__).'/../../../bootstrap/tempDir.php');
+
+$t = new lime_test(0, new lime_output_color());
+
+

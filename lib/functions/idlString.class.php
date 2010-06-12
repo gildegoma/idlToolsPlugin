4dfc4a5773bfea7fbaed7a6ad0fb908a12fd8c97
David Jeanmonod <david.jeanmonod@idael.ch>
2010-06-12 12:58:49 +0000

    idlString: new function to switch between cmaleCase, cssStye and underscord

--- lib/functions/idlString.class.php
+++ lib/functions/idlString.class.php
@@ -8,41 +8,125 @@ class idlString extends idlFunction {
 	 * @return string
 	 */
   public static function camelize($text){
-    return sfToolkit::pregtr($text, array('#/(.?)#e' => "'::'.strtoupper('\\1')", '/(^|_|-)+(.)/e' => "strtoupper('\\2')"));
+    $type = self::guestStringType($text);
+    if ($type == 'invalid'){
+      throw new Exception("Impossible to convert string [$text] to css style");
+    } 
+    else if ($type == 'notype'){
+      return ucfirst($text);
+    }
+    else if ($type == 'css') {
+      $words = explode('-',$text);
+    }
+    else if ($type == 'underscore') {
+      $words = explode('_',$text);
+    }
+    else {
+      // Already a css style string
+      return $text;
+    }
+    $camelString = "";
+    foreach ($words as $word){
+      $camelString .= ucfirst($word);
+    }
+    return $camelString;
   }
+  
+  /**
+   * Underscorify a string to from underscord or camelize string to css-selector style
+   * @param string $text 
+   * @return string
+   */
+  public static function underscorify($text){
+    $type = self::guestStringType($text);
+    if ($type == 'invalid'){
+      throw new Exception("Impossible to convert string [$text] to unerscored style");
+    } 
+    else if ($type == 'notype'){
+      return $text;
+    }
+    else if ($type == 'css') {
+      return str_replace('-', '_', $text);
+    }
+    else if ($type == 'camel') {
+      $text = ucfirst($text); 
+      preg_match_all('/([A-Z][a-z]*)|(\d+)/',$text,$match);
+      return strtolower(implode('_',$match[0]));
+    }
+    else {
+      // Already a css style string
+      return $text;
+    }
+  }
+    
+  
+  /**
+   * cssify
+   * @param string $text 
+   * @return string
+   */
+  public static function cssify($text){
+    $type = self::guestStringType($text);
+    if ($type == 'invalid'){
+      throw new Exception("Impossible to convert string [$text] to css style");
+    } 
+    else if ($type == 'untyped'){
+      return $text;
+    }
+    else if ($type == 'underscore') {
+      return str_replace('_', '-', $text);
+    }
+    else if ($type == 'camel') {
+      $text = ucfirst($text); 
+      preg_match_all('/([A-Z][a-z]*)|(\d+)/',$text,$match);
+      return strtolower(implode('-',$match[0]));
+    }
+    else {
+      // Already a css style string
+      return $text;
+    }
+  }
+  
+  /**
+   * Try to detect the type of a string. Throw an exception if the test is wrong
+   * @return string      Can be css, camel, underscore or untyped
+   */
+  public static function guestStringType($text){
+    // First raw validation
+    if (preg_match("/[^a-z0-9_\-]/i",$text)){
+      throw new Exception("Invalid caracter found in the string identifier: ".$text);
+    }
+    // Guessing type
+    if (preg_match("/^[a-z][a-z]*$/",$text)) {
+      return 'untyped';
+    }
+    else if (preg_match("/^[a-zA-Z][a-zA-Z0-9]*$/",$text)) {
+      return 'camel';
+    }
+    else if (preg_match("/^[a-z]([a-z])*(\-([a-z]+|\d+))*$/",$text)) {
+      return 'css';
+    }
+    else if (preg_match("/^[a-z]([a-z])*(\_([a-z]+|\d+))*$/",$text)) {
+      return 'underscore';
+    }
+    else {
+      return 'invalid';
+    }
+  }
+  
+  
   
   /**
    * Convert camelized string in css style string
    * Example: "MyClassName" is converted to "my-class-name"
    * Only letters and numbers are allowed all other chars will raise en exception
    * @param string $text The string to convert 
+   * @deprecated  Must use the generic method cssify
    * @return string The string converted in css style
    */
   public static function convertCamelizedToCssStyle($text){
-    
-    if (preg_match("/[^a-z0-9]/i",$text)){
-      throw new Exception("Only letters or numbers are allowed in camelized name");
-    }
-    //convert the string to an array
-    $text = str_split($text);
-    
-    //lowcase the first char
-    $text[0]=strtolower($text[0]);
-    $ret = "";
-    foreach ($text as $k=>$c){
-      if (ctype_upper($c)) $c = "-".strtolower($c);
-      $ret .= $c;
-    }
-    return $ret;
-  } 
-  
-    public static function convertUnderScoreToCssStyle($text){
-    
-    return "Todo";
-  } 
-  
-  
-  
+    return self::cssify($text);
+  }   
   
    
 }
--- test/unit/lib/functions/idlStringTest.php
+++ test/unit/lib/functions/idlStringTest.php
@@ -1,27 +1,55 @@
 <?php
 
 include_once(dirname(__FILE__).'/../../../bootstrap/unit.php');
-$t = new lime_test(0, new lime_output_color());
-/*
+$t = new lime_test(35, new lime_output_color());
+
 // Type detection
-$t->is(idlString::guestStringType('simple'), 'notype', "String type detection for simple string");
-$t->is(idlString::guestStringType('MyTestString'), 'camel', "String type detection for CamelCase");
-$t->is(idlString::guestStringType('myTestString'), 'camel', "String type detection for CamelCase");
-$t->is(idlString::guestStringType('my-test-string'), 'css', "String type detection for CSS");
-$t->is(idlString::guestStringType('my_test_string'), 'underscore', "String type detection for Underscored");
+$t->is(idlString::guestStringType('simple'), 'untyped', "->guestStringType() String type detection for simple string");
+$t->is(idlString::guestStringType('Simple'), 'camel', "->guestStringType() String type detection for one word CamelCase");
+$t->is(idlString::guestStringType('MyTestString'), 'camel', "->guestStringType() String type detection for CamelCase");
+$t->is(idlString::guestStringType('myTestString'), 'camel', "->guestStringType() String type detection for CamelCase");
+$t->is(idlString::guestStringType('my404TestString'), 'camel', "->guestStringType() String type detection for CamelCase");
+$t->is(idlString::guestStringType('my-test-string'), 'css', "->guestStringType() String type detection for CSS");
+$t->is(idlString::guestStringType('my-404-test-string'), 'css', "->guestStringType() String type detection for CSS");
+$t->is(idlString::guestStringType('my_test_string'), 'underscore', "->guestStringType() String type detection for Underscored");
+$t->is(idlString::guestStringType('my_404_test_string'), 'underscore', "->guestStringType() String type detection for Underscored");
+$t->is(idlString::guestStringType('404testString'), 'invalid', "->guestStringType() Make as unkown string starting by number");
+$t->is(idlString::guestStringType('my_404test_string'), 'invalid', "->guestStringType() Make as unkown string with number and chars in the same word");
+$t->is(idlString::guestStringType('my_404_Toto'), 'invalid', "->guestStringType() Make as unkown string with upper case and _");
+$t->is(idlString::guestStringType('my-404-Toto'), 'invalid', "->guestStringType() Make as unkown string with upper case and -");
 
+// Camelizing
+//$t->is(idlString::camelize('string'), "String", "->camelize() : String with only one word");
+$t->is(idlString::camelize("my-404-test-string"), 'My404TestString', "->camelize() : From css style");
+$t->is(idlString::camelize("my-t-string"), 'MyTString', "->camelize() : From css style");
+$t->is(idlString::camelize('my_test_string'), "MyTestString", "->camelize() : From underscored ");
+try{idlString::camelize("myString_24;"); $t->fail("->camelize() : must not accept illegal chars!");}
+catch (Exception $e){$t->pass("->camelize() : raise an Exception when not allowed characters are used");}
 
+// CSS Style
+$t->is(idlString::cssify('string'), "string", "->cssify() : String with only one word");
+$t->is(idlString::cssify('String'), "string", "->cssify() : String with only uppercase first");
+$t->is(idlString::cssify('My404TestString'), "my-404-test-string", "->cssify() : From camelCase to css style");
+$t->is(idlString::cssify('myTString'), "my-t-string", "->cssify() : From camelCase with double uppercase to css style");
+$t->is(idlString::cssify('my_test_string'), "my-test-string", "->cssify() : From underscored to css style");
+try{idlString::cssify("myString_24;"); $t->fail("->cssify() : must not accept illegal chars!");}
+catch (Exception $e){$t->pass("->cssify() : raise an Exception when not allowed characters are used");}
 
-$t->is(idlString::convertCamelizedToCssStyle('MyTestString'), "my-test-string", "->convertCamelizedToCssStyle() : Camelize string starting by an uppercase");
-$t->is(idlString::convertCamelizedToCssStyle('myTestString'), "my-test-string", "->convertCamelizedToCssStyle() : Camelize string starting by a lowercase");
-$t->is(idlString::convertCamelizedToCssStyle('myTString'), "my-t-string", "->convertCamelizedToCssStyle() : Camelize string with double uppercase");
-$t->is(idlString::convertCamelizedToCssStyle('String'), "string", "->convertCamelizedToCssStyle() : String with only one word");
-$t->is(idlString::convertCamelizedToCssStyle('string'), "string", "->convertCamelizedToCssStyle() : String with only lowercase");
-try{idlString::convertCamelizedToCssStyle("MyString:Test24;"); $t->fail("->convertCamelizedToCssStyle: must not accept illegal chars!");}
-catch (Exception $e){$t->pass("->convertCamelizedToCssStyle: raise an Exception when not allowed characters are used");}
+// Underscored 
+$t->is(idlString::underscorify('string'), "string", "->underscorify() : String with only one word");
+$t->is(idlString::underscorify('String'), "string", "->underscorify() : String with only uppercase first");
+$t->is(idlString::underscorify('My404TestString'), "my_404_test_string", "->underscorify() : From camelCase to css style");
+$t->is(idlString::underscorify('myTString'), "my_t_string", "->underscorify() : From camelCase with double uppercase to css style");
+$t->is(idlString::underscorify('my-test-string'), "my_test_string", "->underscorify() : From underscored to css style");
+try{idlString::cssify("myString_24;"); $t->fail("->underscorify() : must not accept illegal chars!");}
+catch (Exception $e){$t->pass("->underscorify() : raise an Exception when not allowed characters are used");}
 
-$t->is(idlString::convertUnderScoreToCssStyle('my_test_string'), "my-test-string", "->convertUnderScoreToCssStyle() : Underscore with two underscore");
-$t->is(idlString::convertUnderScoreToCssStyle('my_string'), "my-t-string", "->convertUnderScoreToCssStyle() : Underscore with two underscore");
-try{idlString::convertUnderScoreToCssStyle("_my_string"); $t->fail("->convertUnderScoreToCssStyle: must not accept string that's start with underscore");}
-catch (Exception $e){$t->pass("->convertUnderScoreToCssStyle: refuse string that start with under score");}
-*/
+// Deprecated test, could be remove when removing convertCamelizedToCssStyle
+$t->is(idlString::convertCamelizedToCssStyle('MyTestString'), "my-test-string", "->convertCamelizedToCssStyle() : Camelize string starting by an uppercase"); 
+$t->is(idlString::convertCamelizedToCssStyle('myTestString'), "my-test-string", "->convertCamelizedToCssStyle() : Camelize string starting by a lowercase"); 
+$t->is(idlString::convertCamelizedToCssStyle('myTString'), "my-t-string", "->convertCamelizedToCssStyle() : Camelize string with double uppercase"); 
+$t->is(idlString::convertCamelizedToCssStyle('String'), "string", "->convertCamelizedToCssStyle() : String with only one word"); 
+$t->is(idlString::convertCamelizedToCssStyle('string'), "string", "->convertCamelizedToCssStyle() : String with only lowercase"); 
+try{idlString::convertCamelizedToCssStyle("MyString:Test24;"); $t->fail("->convertCamelizedToCssStyle() : must not accept illegal chars!");} 
+catch (Exception $e){$t->pass("->convertCamelizedToCssStyle() : raise an Exception when not allowed characters are used");} 
+

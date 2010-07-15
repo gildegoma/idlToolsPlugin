ad0326f2f080760e4952398a2be1193dda895b70
David Jeanmonod <david.jeanmonod@idael.ch>
2010-07-15 10:33:36 +0000

    New class for easier logging

---
+++ lib/logger/idlLogger.class.php
@@ -0,0 +1,76 @@
+<?php
+
+class idlLogger {
+  
+  const ERROR   = 0; // Error
+  const INFO    = 1; // Important information
+  const DEBUG   = 2; // Message that will be availliable only on debug mode
+  const DEV     = 3; // Dev message, should not perssite after dev phase finalize
+
+  private static $singleton;
+  private $logger;
+  private $logPrefix;
+  
+  // Private constructor as it's a singleton
+  private function __construct(){
+    if (class_exists('sfContext') && sfContext::hasInstance()){
+      $this->logger = sfContext::getInstance()->getLogger();
+    }
+    $this->logPrefix = 'IDL: '; // TODO Should be configurable from the app.yml 
+  }
+  
+  public static function getInstance(){
+    if ( ! self::$singleton ){
+      self::$singleton = new idlLogger();
+    }
+    return self::$singleton;
+  }
+  
+  // To fast log dev message, should not perssite after dev phase finalize 
+  public static function dev($msg){
+    self::getInstance()->log($msg, self::DEV);
+  }
+  
+  // To log message that will be availliable only on debug mode
+  public static function debug($msg){
+    self::getInstance()->log($msg, self::DEBUG);
+  }
+  
+  // To log an information message
+  public static function info($msg){
+    self::getInstance()->log($msg, self::INFO);
+  }
+  
+  // To log an imporant error
+  public static function error($msg){
+    self::getInstance()->log($msg, self::ERROR);
+  }
+  
+  public function log($msg, $type){
+    
+    // If no intenal logger define, just return
+    if (!isset($this->logger)) return;
+    
+    switch ($type) {
+    case self::DEV:
+      if ( sfConfig::get('sf_environment', 'prod') !='dev' ) {
+        return; 
+      }
+      $sfLoggerPriority = sfLogger::DEBUG;
+      break;
+    case self::DEBUG:
+      $sfLoggerPriority = sfLogger::DEBUG;
+      break;
+    case self::INFO:
+      $sfLoggerPriority = sfLogger::INFO;
+      break;
+    case self::ERROR:
+      $sfLoggerPriority = sfLogger::ERR;
+      break;
+    }
+    
+    $this->logger->log($this->logPrefix.$msg, $sfLoggerPriority);
+    
+  }
+    
+}

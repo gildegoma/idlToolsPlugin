64b6ffb9aac52c950c8d4c17eeaf555fa2eae8cd
David Jeanmonod <david.jeanmonod@idael.ch>
2010-09-14 20:25:26 +0000

    idlDateTest: little fix

--- test/unit/lib/functions/idlDateTest.php
+++ test/unit/lib/functions/idlDateTest.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once(dirname(__FILE__).'/../../../bootstrap/unit.php');
-$t = new lime_test(2, new lime_output_color());
+$t = new lime_test(3, new lime_output_color());
 
 $t->is(idlDate::toTS("1980-11-08"), 342486000, "toTS() Convert date to TS");
 $t->is(idlDate::toTS("1515-1-1"), -14358474000, "toTS() Convert big date to TS");

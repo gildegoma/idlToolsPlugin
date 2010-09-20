eb0b4fd278801e5fa03740e4439aacd2e889dbc4
David Jeanmonod <david.jeanmonod@idael.ch>
2010-09-20 20:19:15 +0000

    idlDate::toTS() Add possibility to return integer instead of string

--- lib/functions/idlDate.class.php
+++ lib/functions/idlDate.class.php
@@ -4,12 +4,25 @@ class idlDate extends idlFunction {
 	
 	/**
 	 * Return a TS of the provided date
+	 *  By default the timestamp is return as a string to avoid int overflow problem. But, you can request 
+	 *   to get the value as int. At that moment, if the date could create an int overflow, then an exception
+	 *   is thrown
    * @param string $date date
-	 * @return integer
+	 * @return string|int
 	 */
-	public static function toTS($date){
+	public static function toTS($date, $useInt=false){
 	  $date = new DateTime($date);
-		return $date->format('U');    
+	  // Return as a string
+	  if ($useInt==false){
+		  return $date->format('U');
+	  }
+	  // Try to return as a string
+	  $strVal = $date->format('U');
+	  $PHP_INT_MIN = (int)(PHP_INT_MAX+1);
+	  if ($strVal > PHP_INT_MAX || $strVal < $PHP_INT_MIN){
+	    throw new Exception("Impossible to convert the date $date in integer timestamp, as the current system limitations are reach");
+	  }
+    return (int) $date->format('U');
 	}
 
 }
--- test/unit/lib/functions/idlDateTest.php
+++ test/unit/lib/functions/idlDateTest.php
@@ -1,8 +1,19 @@
 <?php
 
 include_once(dirname(__FILE__).'/../../../bootstrap/unit.php');
-$t = new lime_test(3, new lime_output_color());
+$t = new lime_test(6, new lime_output_color());
 
 $t->is(idlDate::toTS("1980-11-08"), 342486000, "toTS() Convert date to TS");
 $t->is(idlDate::toTS("1515-1-1"), -14358474000, "toTS() Convert big date to TS");
 $t->is(idlDate::toTS("6099-12-31"), 130330162800, "toTS() Convert small date to TS");
+$t->ok(is_string(idlDate::toTS("2009-12-31")), "toTS() By default, return a string");
+$t->ok(is_int(idlDate::toTS("2009-12-31", true)), "toTS() Can return int if requested");
+
+// Test about convert to int on 32bit system
+if (PHP_INT_MAX==2147483647){
+  try{idlDate::toTS("1515-1-1", true); $t->fail("->toTS() Fail to raise exception if the int min/max value are reach");}
+  catch (Exception $e){$t->pass("->toTS() Request int conversion raise exception if the int min/max value are reach");}
+}
+else {
+  $t->skip('Convert out of bound, no testable on 64bit system');
+}

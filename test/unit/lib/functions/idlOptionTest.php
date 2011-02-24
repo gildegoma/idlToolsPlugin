23027386b47a1ac8046c31747a3114427eb3094a
Lazhar Farjallah <lazhar.farjallah@idael.ch>
2011-02-24 23:02:08 +0000

    ~ Adding a validate() function for checking arrays of params with options that is either optional or mandatory  ~ Adding 3 new unit tests for the new validate() function

--- test/unit/lib/functions/idlOptionTest.php
+++ test/unit/lib/functions/idlOptionTest.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once(dirname(__FILE__).'/../../../bootstrap/unit.php');
-$t = new lime_test(3, new lime_output_color());
+$t = new lime_test(6, new lime_output_color());
 
 $default = array('op1' => 1, 'op2' => 2, 'op3' => 3);
 
@@ -26,3 +26,35 @@ try {
 catch (Exception $e) {
   $t->fail("merge() Refuse a non default option, but that was explicitely autorized");
 }
+
+
+// Test validate() with normal things
+$paramList = array('op1', 'op2', 'op3');
+try {
+  idlOption::validate($paramList, array('*op1', 'op2', 'op3'));
+  $t->pass('->validate() succeeded');
+}
+catch(Exception $e){
+  $t->fail("->validate() does not succeeded: ".$e->getMessage());
+}
+
+// Test validate() with with 1 extra parameter that is nor optional neither mandatory
+$paramList = array('op1', 'op2', 'op3', 'op4');
+try {
+  idlOption::validate($paramList, array('*op1', 'op2', 'op3'));
+  $t->fail('->validate() must not succeed');
+}
+catch(Exception $e){
+  $t->pass('->validate() did not succeed because 1 extra parameter that is nor optional neither mandatory was given');
+}
+
+// Test validate() with 1 parameter that is mandatory but not given
+$paramList = array('op1', 'op2');
+try {
+  idlOption::validate($paramList, array('*op1', 'op2', 'op3'));
+  $t->fail('->validate() must not succeed');
+}
+catch(Exception $e){
+  $t->pass('->validate() did not succeed because 1 parameter that is mandatory is not given');
+}
+
